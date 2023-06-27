78754525d7036f8a6370c39bd3a0371417509885
LoginamNet <loginamnet@gmail.com>
2023-06-27 15:14:04 +0300

    refactor: refactor header and footer

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -2,17 +2,19 @@ import './footer.css';
 
 function Footer() {
   return (
-    <footer className="footer text-color-peperment">
-      <span>2023 by Aleksandr Chuchev</span>
-      <div className="footer_social-icons">
-        <div
-          className="socilal-icon socilal-icon_github"
-          onClick={() => (window.location.href = 'https://github.com/LoginamNet')}
-        ></div>
-        <div
-          className="socilal-icon socilal-icon_discord"
-          onClick={() => (window.location.href = 'https://discordapp.com/users/loginamnet')}
-        ></div>
+    <footer className="footer section-dark">
+      <div className="footer-box">
+        <span className="text-color-peperment">2023 by Aleksandr Chuchev</span>
+        <div className="footer-box__social-icons">
+          <div
+            className="socilal-icon socilal-icon__github"
+            onClick={() => (window.location.href = 'https://github.com/LoginamNet')}
+          ></div>
+          <div
+            className="socilal-icon socilal-icon__discord"
+            onClick={() => (window.location.href = 'https://discordapp.com/users/loginamnet')}
+          ></div>
+        </div>
       </div>
     </footer>
   );
--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -5,17 +5,19 @@ import './header.css';
 function Header() {
   return (
     <header className="header section-dark text-color-white">
-      <Link to="/" className="menu-link text-color-white">
-        CV
-      </Link>
-      <nav className="header-menu">
+      <div className="header-box">
         <Link to="/" className="menu-link text-color-white">
-          Projects
+          CV
         </Link>
-        <Link to="/1" className="menu-link text-color-white">
-          Contacts
-        </Link>
-      </nav>
+        <nav className="header-menu">
+          <Link to="/" className="menu-link text-color-white">
+            Projects
+          </Link>
+          <Link to="/1" className="menu-link text-color-white">
+            Contacts
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
