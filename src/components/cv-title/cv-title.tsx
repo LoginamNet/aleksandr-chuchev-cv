a3a9355fa78e1ccfc17f8c5ae4551b7265cec323
LoginamNet <loginamnet@gmail.com>
2023-07-18 16:10:56 +0300

    refactor title text appearence

--- src/components/cv-title/cv-title.tsx
+++ src/components/cv-title/cv-title.tsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react';
 import './cv-title.css';
 
 function Title() {
+  const [text, setText] = useState('');
+  const [index, setIndex] = useState(0);
+
+  const fullText = 'Aleksandr Chuchev';
+
+  useEffect(() => {
+    if (index < fullText.length) {
+      setTimeout(() => {
+        setText(text + fullText[index]);
+        setIndex(index + 1);
+      }, 70);
+    }
+  }, [fullText, index, text]);
+
   return (
     <section className="section section-white">
       <div className="wrapper cv-title">
         <div className="cv-title__text-box">
-          <h1 className="small-text text-color-blue">Aleksandr Chuchev</h1>
+          <h1 className="small-text text-color-blue">
+            {text}
+            <span className="cv-title__cursor">|</span>
+          </h1>
           <span className="cv-title__text text-color-dark">Portfolio</span>
         </div>
       </div>
