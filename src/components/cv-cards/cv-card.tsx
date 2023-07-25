68e5fe06fe2b0d8d640bcf73701c2c8f29eb03ff
LoginamNet <loginamnet@gmail.com>
2023-07-25 21:55:16 +0300

    refactor: try to add some scroll effects

--- src/components/cv-cards/cv-card.tsx
+++ src/components/cv-cards/cv-card.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState, useEffect } from 'react';
 import './cv-card.css';
 
 type ComponentProps = {
@@ -6,9 +7,36 @@ type ComponentProps = {
 };
 
 function CVCard(props: ComponentProps) {
+  const elementRef = useRef<HTMLDivElement>(null);
+  const [position, setPosition] = useState({ y: 0, height: 0 });
+
+  const handleScroll = () => {
+    if (elementRef.current !== null) {
+      setPosition({
+        y: elementRef.current.getBoundingClientRect().top,
+        height: elementRef.current.getBoundingClientRect().height,
+      });
+    }
+  };
+
+  useEffect(() => {
+    window.removeEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    handleScroll();
+  }, []);
+
   return (
-    <div className="cv-card">
-      <div className="cv-card__image" style={{ backgroundImage: `url(${props.screenshot})` }}></div>
+    <div className="cv-card" ref={elementRef}>
+      <div
+        className={`cv-card__image ${
+          position.y < position.height / 2 && 'cv-card__image-scrolled'
+        }`}
+        style={{ backgroundImage: `url(${props.screenshot})` }}
+      ></div>
       <span className="cv-card__description text-color-white ">{props.description}</span>;
     </div>
   );
--- src/components/cv-title/cv-title.tsx
+++ src/components/cv-title/cv-title.tsx
@@ -6,6 +6,7 @@ import './cv-title.css';
 function CVTitle() {
   const [text, setText] = useState('');
   const [index, setIndex] = useState(0);
+  const [offset, setOffset] = useState(0);
 
   const fullText = 'Aleksandr Chuchev';
 
@@ -18,9 +19,18 @@ function CVTitle() {
     }
   }, [fullText, index, text]);
 
+  useEffect(() => {
+    const onScroll = () => setOffset(window.scrollY);
+
+    window.removeEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
     <section className="section section-white">
       <div className="wrapper cv-title">
+        <div className="cv-title__rotate" style={{ transform: `rotate(${offset / 15}deg)` }}></div>
         <div className="cv-title__text-box">
           <span className="cv-title__text text-color-dark">Front-end Developer</span>
           <h1 className="small-text text-color-blue">
@@ -32,8 +42,6 @@ function CVTitle() {
           contact me
           <svg width="25" height="6" viewBox="0 0 25 6" fill="none">
             <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
               d="M21.2235 2.18487V0L24.9689 2.80899L21.2235 5.61798V3.43326H0V2.18487H21.2235Z"
               fill="currentColor"
             />
