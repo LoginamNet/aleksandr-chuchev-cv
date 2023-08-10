7770f2cacc4b66eca548473e52608ed9eecd44c1
LoginamNet <loginamnet@gmail.com>
2023-08-10 11:51:37 +0300

    feat: add random projects appearence on main page

--- src/components/cv-cards/cv-cards.tsx
+++ src/components/cv-cards/cv-cards.tsx
@@ -5,11 +5,13 @@ import PROJECTS_DATA from '../../constants/projects-data';
 import './cv-cards.css';
 
 function CVCards() {
+  const RANDOM_PROJECTS = PROJECTS_DATA.slice().sort(() => 0.5 - Math.random());
+
   return (
     <section className="section section-dark" id="projects">
       <div className="wrapper cv-cards">
         <div className="cv-cards__box">
-          {PROJECTS_DATA.slice(0, 3).map((el, key) => (
+          {RANDOM_PROJECTS.slice(0, 3).map((el, key) => (
             <CVCard
               key={key}
               screenshot={el.screenshot}
@@ -23,7 +25,7 @@ function CVCards() {
           className="button-rounded cv-cards__button"
           onClick={() => window.scrollTo(0, 0)}
         >
-          get more of them
+          get more
           <svg width="25" height="6" viewBox="0 0 25 6" fill="none">
             <path
               d="M21.2235 2.18487V0L24.9689 2.80899L21.2235 5.61798V3.43326H0V2.18487H21.2235Z"
