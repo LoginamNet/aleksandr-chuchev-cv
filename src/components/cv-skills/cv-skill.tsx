8dba8749362334125db8d2a07f05b7e9e6bd1e6c
LoginamNet <loginamnet@gmail.com>
2023-08-11 12:20:24 +0300

    feat: add hover animation to cv skills

--- src/components/cv-skills/cv-skill.tsx
+++ src/components/cv-skills/cv-skill.tsx
@@ -7,7 +7,9 @@ type ComponentProps = {
 function Skill(props: ComponentProps) {
   return (
     <div className="cv-skill">
+      {props.name}
       <span className="cv-skill-text">{props.name}</span>
+      <span className="cv-skill-text cv-skill-text__hidden">{props.name}</span>
     </div>
   );
 }
