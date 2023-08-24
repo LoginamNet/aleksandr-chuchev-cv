5a5f03baebe2b43dfce0d67126ff267ad6210f3a
LoginamNet <loginamnet@gmail.com>
2023-08-24 09:17:48 +0300

    refactor: refactor font sizes & add download button

--- src/components/cv-title/cv-title.tsx
+++ src/components/cv-title/cv-title.tsx
@@ -67,15 +67,18 @@ function CVTitle() {
             />
           </div>
         </div>
-        <HashLink to="#contacts" className="button-rounded cv-title__button">
-          contact me
-          <svg width="25" height="6" viewBox="0 0 25 6" fill="none">
-            <path
-              d="M21.2235 2.18487V0L24.9689 2.80899L21.2235 5.61798V3.43326H0V2.18487H21.2235Z"
-              fill="currentColor"
-            />
-          </svg>
-        </HashLink>
+        <div className="cv-title__buttons">
+          <button className="button-rounded cv-title__download">download cv</button>
+          <HashLink to="#contacts" className="button-rounded cv-title__button">
+            contact me
+            <svg width="25" height="6" viewBox="0 0 25 6" fill="none">
+              <path
+                d="M21.2235 2.18487V0L24.9689 2.80899L21.2235 5.61798V3.43326H0V2.18487H21.2235Z"
+                fill="currentColor"
+              />
+            </svg>
+          </HashLink>
+        </div>
       </div>
     </section>
   );
--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -13,25 +13,25 @@ function Header() {
   }, [location]);
 
   return (
-    <header className="header section-dark text-color-white">
+    <header className="header section-white">
       <div className="header-box">
         <nav className={url !== '/' ? 'non-displayed' : 'header-menu'}>
-          <HashLink to="#about" className="menu-link menu-link__underline text-color-white">
+          <HashLink to="#about" className="menu-link menu-link__underline text-color-dark">
             About
           </HashLink>
-          <HashLink to="#projects" className="menu-link menu-link__underline text-color-white">
+          <HashLink to="#projects" className="menu-link menu-link__underline text-color-dark">
             Projects
           </HashLink>
-          <HashLink to="#skills" className="menu-link menu-link__underline text-color-white">
+          <HashLink to="#skills" className="menu-link menu-link__underline text-color-dark">
             Skills
           </HashLink>
-          <HashLink to="#contacts" className="menu-link menu-link__underline text-color-white">
+          <HashLink to="#contacts" className="menu-link menu-link__underline text-color-dark">
             Contacts
           </HashLink>
         </nav>
         <nav className={url === '/' ? 'non-displayed' : 'header-menu'}>
-          <Link to="/" className="menu-link menu-link__underline text-color-white">
-            CV
+          <Link to="/" className="menu-link menu-link__underline text-color-dark">
+            Back to CV
           </Link>
         </nav>
       </div>
