57e58c4c0f19ce0f4b0f56a5abebfce4eae31ac7
LoginamNet <loginamnet@gmail.com>
2023-08-28 13:30:04 +0300

    feat: add new favicon fix caroosel width

--- src/components/cv-caroosel/cv-caroosel.tsx
+++ src/components/cv-caroosel/cv-caroosel.tsx
@@ -7,7 +7,7 @@ function Caroosel() {
   const RANDOM_SKILLS = CV_DATA.skills.slice().sort(() => 0.5 - Math.random());
 
   return (
-    <section className="section section-dark">
+    <section className="section section-dark section-full-width">
       <Marquee className="cv-caroosel" speed={75} autoFill>
         {RANDOM_SKILLS.map((el, key) => (
           <div
